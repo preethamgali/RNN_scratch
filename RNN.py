ef11365c987514a3fd3b1b754a81d424650359a1
Preetham Gali <preethamgali007@gmail.com>
2019-11-10 03:05:21 +0530

    name prediction using RNN

--- RNN.py
+++ RNN.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 alphabets = "abcdefghijklmnopqrstuvwxyz#"
-def one_hot_endcode(name):
+def one_hot_encode(name):
     name = name.lower()
     data = []
     name = name+'#'
@@ -11,13 +11,29 @@ def one_hot_endcode(name):
         data.append(temp)
     return data
 
+def get_letter(data):
+    return alphabets[np.argmax(data)]
+
 def normalize_data(data):
     data  = data - np.mean(data)
     data/=np.max(data)
     return data
 
-def get_letter(data):
-    return alphabets[np.argmax(data)]
+def data_set():
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    with open("IndianNames.csv", encoding="utf8") as f:
+        names = []
+        for name in f.read().split():        
+            good_name = True
+            for c in name.lower():
+                if c not in alpha:
+                    good_name = False
+
+            if good_name:
+                names.append(name)
+                
+    from collections import Counter
+    return list(dict(Counter(names)).keys())
 
 def softmax(data):
     data = data - np.max(data)
@@ -26,7 +42,7 @@ def softmax(data):
     return out
 
 def tanh_prime(data):
-#     data is tanh ouput
+#   data is tanh output
     return 1-data**2
 
 def cross_entropy(c_output,a_output):
@@ -35,174 +51,91 @@ def cross_entropy(c_output,a_output):
 def dsoftmax_cross_entropy(c_output,a_output):
     return c_output - a_output
 
-input_size = 26+1
-hidden_size = input_size+3
-output_size = input_size
+wx = np.random.normal(0,1,(27,30))
+ws = np.random.normal(0,1,(30,30))
+wy = np.random.normal(0,1,(30,27))
 
-# 27x30
-U = np.random.normal(0,1,(input_size,hidden_size))
+def train(wx,wy,ws,name):
 
-# 30x30
-W = np.random.normal(0,1,(hidden_size,hidden_size))
-
-# 30x27
-V = np.random.normal(0,1,(hidden_size,output_size))
-
-def train(U,V,W,name):
-    import time
-
-    start = time.time()
-    h_t, h_activation_t, h_out_t, h_out_activation_t, yhat_t = [],[],[],[],[]
-    dEdV_t, dEdW_t, dEdU_t = np.zeros_like(V),np.zeros_like(W),np.zeros_like(U)
-
-    name = name
-    data = one_hot_endcode(name)
-    x = data[0]
-    x_t = []
-    x_t.append(x)
-    y_t = data[1:]
-
-    """
-
-            yhat                     yhat
-            ^                        ^
-            |                        |           
-          h_out_activation      h_out_activation
-          h_out                    h_out 
-            ^                        ^
-            | V            W         | V            W
-          h_activation ------>  h_activation  ------>  
-            h                        h
-            ^                        ^
-            | U                      | U  
-            x_t                      x_t
-
-
-    """
-
-    mess = ""
-    # forward pass
+    xt = one_hot_encode(name)[:-1]
+    yt = one_hot_encode(name)[1:]
     error = 0
-    for i in range(len(y_t)):
+    
+    #     forward pass
+    st_p = [np.zeros((1,30))]
+    yhatt = []
+    
+    for i in range(len(xt)):
+        s = np.tanh(np.matmul(xt[i],wx)+np.matmul(st_p[i],ws))
+        st_p.append(s)
+        yhat = softmax(np.matmul(s,wy))
+        yhatt.append(yhat)
+           
+        error += np.sum(cross_entropy(yhat,xt[i]))
 
-        # 1x27 --> 1x30
-        h = np.matmul(x,U) if i==0 else np.matmul(x,U)+np.matmul(h_activation_t[i-1],W)
-        h_activation = np.tanh(h)
-        # 1x30 --> 1x27
-        h_out = np.matmul(h_activation,V)
-        h_out_activation = np.tanh(h_out)
-        yhat = softmax(h_out_activation)
-        x = one_hot_endcode(alphabets[np.argmax(yhat)])[0]
-        x_t.append(x)
-        error+= cross_entropy(yhat,y_t[i])
-            
-        h_activation_t.append(h_activation)
-        h_out_activation_t.append(h_out_activation)
-        yhat_t.append(yhat)
-        mess = mess+get_letter(yhat)
+    #       BPTT
+    dEdyt = [];dsds = [];dEdst = []
 
-#     print("=======>",error)
-    print(mess)
-
-    dh_activation_dW_t = []
-    dh_activation_dU_t = []
-    dh_activation_dh_t = []
-    dh_activation_dh_activation_pre = []
-
-    temp = W
-    for i in range(len(x_t)-1):
-        dh_activation_dh_activation_pre.append(temp)
-        temp = np.matmul(temp,W)
-
-    # back prop
-    for i in range(len(y_t)):
-
-        # dE/dh out activation
-        dE_dh_out_activation = dsoftmax_cross_entropy(yhat_t[i],y_t[i])
-        # dh out activation/dh out
-        dh_out_activation_dh_out = tanh_prime(h_out_activation_t[i])
-        # dE/dh out
-        dE_dh_out = dh_out_activation_dh_out*dE_dh_out_activation
+    temp = ws
+    for i in range(len(xt)-1):
+        dsds.append(temp)
+        temp = np.matmul(ws,temp)
         
+    dsds = [normalize_data(data) for data in dsds]
 
-        # dh out/dV
-        dh_out_dV = h_activation_t[i]
-        #dh out/dh activaion
-        dh_out_dh_activation = V
-        
+    dwy = np.zeros_like(wy)
+    dwx = np.zeros_like(wx)
+    dws = np.zeros_like(ws)
+    alpha = 0.0005
 
-        # dE/dh activation
-        dE_dh_activation = np.matmul(dh_out_dh_activation,(dE_dh_out).T).T
-        # dh_activation_dh_activation_pre = np.zeros_like(W) if i==0 else W
+    for i in range(len(xt)):
+        dEdy = dsoftmax_cross_entropy(yhatt[i],yt[i])
+        dEdyt.append(dEdy)
 
-        # dh activation/dh
-        dh_activation_dh = tanh_prime(h_activation_t[i])
-        dh_activation_dh_t.append(dh_activation_dh)
-        # dh/dU
-        dh_dU = x_t[i]
+        dEdwy = np.matmul(st_p[i+1].T,dEdy)
 
-        # dh activation/dcU
-        dh_activation_current_dU = np.matmul(dh_dU.T,dh_activation_dh)
-        # dh activation/dcW
-        dh_activation_current_dW = np.zeros_like(h_activation_t[0]) if i==0 else h_activation_t[i-1]
-        # dh activation/dW
-        dh_activation_dW = dh_activation_current_dW
-        # dh activation/dU
-        dh_activation_dU = dh_activation_current_dU
+        dEds = np.matmul(dEdy,wy.T)
+        dEdst.append(dEds)
 
-
+        dEdws = np.matmul(st_p[i].T,dEds)
         for j in range(i):
-            dh_activation_dW = dh_activation_dW + np.matmul(dh_activation_dW_t[i-j-1],dh_activation_dh_activation_pre[j])
-            dh_activation_dU = dh_activation_dU + np.matmul(dh_activation_dU_t[i-j-1],dh_activation_dh_activation_pre[j])
-        dh_activation_dW_t.append(dh_activation_dW)
-        dh_activation_dU_t.append(dh_activation_dU)
+            dEdws += np.matmul(st_p[i-1-j].T,np.matmul(dEds,dsds[j].T))
 
-        dE_dV = np.matmul(dh_out_dV.T,dE_dh_out)
-        dE_dW = np.matmul(dE_dh_activation.T,dh_activation_dW)
-        dE_dU = dE_dh_activation*dh_activation_dU
+        dEdwx = np.matmul(xt[i].T,tanh_prime(st_p[i])) * dEds
+        for j in range(i):
+            dEdwx += np.matmul(xt[i-1-j].T,tanh_prime(st_p[i-j])) * np.matmul(dEds,dsds[j].T)
+
+        dwy += dEdwy
+        dwx += dEdwx
+        dws += dEdws
+
+    wx -= alpha * dwx
+    ws -= alpha * dws
+    wy -= alpha * dwy
+
+    return wx,wy,ws
+
+names = data_set()[:20]
+# names = ["preetham","goutham","praveen","gali"]
+for _ in range(1000):
+    for name in names:
+        wx,wy,ws = train(wx,wy,ws,name)
         
-        dEdV_t+=dE_dV
-        dEdW_t+=dE_dW
-        dEdU_t+=dE_dU
+def predict(wx,wy,ws,name):
+    
+    xt = one_hot_encode(name)
+    #     forward pass
+    s = np.zeros((1,30))
+    for i in range(20):
         
+        x = y if i >= len(name) else xt[i]    
+        s = np.tanh(np.matmul(x,wx)+np.matmul(s,ws))
+        y = softmax(np.matmul(s,wy))
+        y = one_hot_encode(get_letter(y))[0]
 
-
-    alpha = 0.01
-    U = normalize_data(U - alpha * dEdU_t)
-    V = normalize_data(V - alpha * dEdV_t)
-    W = normalize_data(W - alpha * dEdW_t)
-    
-    
-    if name+'#' == get_letter(x_t[0]) + mess:
-        print("yaaaaaaaaaaaaaaaaaaaaaaaaa")
-        print(get_letter(x_t[0]) + mess)
-    return U,V,W
-    
-for i in range(100):
-    for name in ["goutham","preetham"]:
-        U,V,W = train(U,V,W,name)
-
-def test(U,V,W,name):
-    
-    name = name
-    data = one_hot_endcode(name)
-    x = data[0]
-
-    mess = get_letter(x)
-    yhat = x
-    # forward pass
-    error = 0
-    h_activation = np.zeros((1,30))
-    while(get_letter(yhat)!='#'):
-        h = np.matmul(x,U)+np.matmul(h_activation,W)
-        h_activation = np.tanh(h)
-        h_out = np.matmul(h_activation,V)
-        h_out_activation = np.tanh(h_out)
-        yhat = softmax(h_out_activation)
-        x = one_hot_endcode(alphabets[np.argmax(yhat)])[0]
-        print(get_letter(yhat),end = "")
-
-#         mess = mess+get_letter(yhat)
-#         print(mess)
-
-test(U,V,W,"g")
+        if get_letter(x) == '#':
+            return
+        
+        print(get_letter(x),end='')
+        
+predict(wx,wy,ws,'r')
